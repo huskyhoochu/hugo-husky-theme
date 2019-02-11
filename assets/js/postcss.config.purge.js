b5f2247b1ba7cf99d11ac4a7da5f60941fbe7ca0
huskyhoochu <dfg1499@gmail.com>
2019-02-11 22:58:03 +0900

    [#2] PurgeCSS 옵션 추가

---
+++ assets/js/postcss.config.purge.js
@@ -0,0 +1,29 @@
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g)
+  }
+}
+
+const themeDir = __dirname + '/../../';
+
+module.exports = {
+  plugins: [
+    require('postcss-import')({
+      path: ['./css'],
+    }),
+    require('tailwindcss')(themeDir + './assets/js/tailwind.js'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [`${themeDir}layouts/**/*.html`],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['html'],
+        }
+      ],
+      fontface: true
+    })
+    require('autoprefixer')({
+      browsers: ['>1%']
+    }),
+  ]
+}
