3d0ef3056cd8afb6191e34d3e2eb3597326dbf24
huskyhoochu <dfg1499@gmail.com>
2019-02-11 23:01:48 +0900

    [#2] 참조 위치 변경

--- assets/js/postcss.config.purge.js
+++ assets/js/postcss.config.purge.js
@@ -13,7 +13,7 @@ module.exports = {
     }),
     require('tailwindcss')(themeDir + './assets/js/tailwind.js'),
     require('@fullhuman/postcss-purgecss')({
-      content: ['layouts/**/*.html'],
+      content: [`${themeDir}layouts/**/*.html`],
       extractors: [
         {
           extractor: TailwindExtractor,
